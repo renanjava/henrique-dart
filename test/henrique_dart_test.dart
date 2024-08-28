8471cd7fc00f8e11f3423a27f5c4f7b8eaa94cf3
renanjava <renancvr777@gmail.com>
2024-08-28 19:26:04 -0300

    adicionando mais testes unitários

--- test/henrique_dart_test.dart
+++ test/henrique_dart_test.dart
@@ -9,4 +9,22 @@ void main() {
     expect(contaFisica.extratoSaldo(), 0);
     expect(contaJuridica.extratoSaldo(), 0);
   });
+
+  test('aumentarSaldo', () {
+    ContaFisica contaFisica = new ContaFisica("000.000.000-00");
+    contaFisica.resgatarValor(100);
+
+    expect(contaFisica.extratoSaldo(), 100);
+  });
+
+  test('transferirValores', () {
+    ContaFisica contaFisica = new ContaFisica("000.000.000-00");
+    ContaJuridica contaJuridica = new ContaJuridica("000.000.000-00");
+
+    contaJuridica.resgatarValor(100);
+    contaJuridica.transferirValor(contaFisica, 50);
+
+    expect(contaFisica.extratoSaldo(), 50);
+    expect(contaJuridica.extratoSaldo(), 50);
+  });
 }
