6a2ab3a5a07498eb2343b7a55ba95fdc1eb22f73
Renan <renancvr777@gmail.com>
2024-08-28 14:51:48 -0300

    criando o projeto em dart

---
+++ bin/henrique_dart.dart
@@ -0,0 +1,5 @@
+import 'package:henrique_dart/henrique_dart.dart' as henrique_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${henrique_dart.calculate()}!');
+}
---
+++ test/henrique_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:henrique_dart/henrique_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
