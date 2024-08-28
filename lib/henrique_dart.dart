db2866136fb6243212d4925318ca85e7f65dfe43
Renan <renancvr777@gmail.com>
2024-08-28 16:00:05 -0300

    add: classes de conta bancaria2

--- lib/henrique_dart.dart
+++ lib/henrique_dart.dart
@@ -1,1 +1,28 @@
+abstract class ContaBancaria {
+  double saldo = 0.00;
 
+  double extratoSaldo() {
+    return this.saldo;
+  }
+
+  void transferirValor<T extends ContaBancaria>(T contaDestino, int valor) {
+    this.saldo -= valor;
+    contaDestino.saldo += valor;
+  }
+
+  void resgatarValor(int valor) {
+    this.saldo += valor;
+  }
+}
+
+class ContaJuridica extends ContaBancaria {
+  String cnpj;
+
+  ContaJuridica(this.cnpj) {}
+}
+
+class ContaFisica extends ContaBancaria {
+  String cpf;
+
+  ContaFisica(this.cpf) {}
+}
